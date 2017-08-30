c2410243de086e858d2b8182dd10e93d7621b313
Andi Andreas <nexuister@gmail.com>
2017-08-29 22:42:28 -0400

    TMDB movie results now render into detailed cards

--- _harp/assets/scripts/search.js
+++ _harp/assets/scripts/search.js
@@ -3,18 +3,35 @@ function fetchJSON(fromURL) {
         .then((res) => {
             // Fetches don't fail if the status code indicates a problem, so we have to check ourselves
             if (res.status >= 200 && res.status < 300) return res;
-            var error = new Error(response.statusText);
-            error.response = response;
+            var error = new Error(res.statusText);
+            error.res = res;
             throw error;
         })
         .then((res) => res.json());
 }
 
+Vue.component("rating-stars", {
+    props: ['rating'],
+    data: function() {
+        console.log(this.rating);
+        console.log(Math.floor(this.rating));
+        return {
+            "fullStars": Math.floor(this.rating),
+            "halfStar": Math.round(this.rating) > Math.floor(this.rating)
+        };
+    },
+    template: `
+        <span>
+            <i class = 'fa fa-star' v-for='x in fullStars'></i><i class = 'fa fa-star-half-o' v-if='halfStar'></i>
+        </span>`
+});
+
 let root = new Vue({
     el: "#root",
     data: {
-        "imdb": "null",
-        "tmdb": "null"
+        "imdb": null,
+        "tmdb": null,
+        "genres": null
     },
     mounted: function() {
         let params = new URLSearchParams(window.location.search);
@@ -22,8 +39,10 @@ let root = new Vue({
             let movie = encodeURIComponent(params.get("q"));
             let tmdbURI =  `https://api.themoviedb.org/3/search/movie?api_key=${tmdbKey}&language=en-US&query=${movie}&page=1&include_adult=false`;            
             let imdbURI = `https://theimdbapi.org/api/find/movie?title=${movie}`;
-            fetchJSON(tmdbURI).then((json) => root.tmdb = json);
+            let genresURI = `https://api.themoviedb.org/3/genre/movie/list?api_key=${tmdbKey}&language=en-US`;
+            fetchJSON(tmdbURI).then((json) => root.tmdb = json.results);
             fetchJSON(imdbURI).then((json) => root.imdb = json);
+            fetchJSON(genresURI).then((json) => root.genres = json);
             
         }
     }
