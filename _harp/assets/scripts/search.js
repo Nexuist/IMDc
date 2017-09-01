f32f9d62fdf4730755e47f6e5e9e0ec20d3b493f
Andi Andreas <nexuister@gmail.com>
2017-08-31 22:53:30 -0400

    Finished pagination functionality

--- _harp/assets/scripts/search.js
+++ _harp/assets/scripts/search.js
@@ -1,4 +1,4 @@
-var tmdbKey = "a487dd6689557f40c93be7ad06f3024f"; // The API key really shouldn't be public but I don't have a backend so ¯\_(ツ)_/¯
+var moviesKey = "a487dd6689557f40c93be7ad06f3024f"; // The API key really shouldn't be public but I don't have a backend so ¯\_(ツ)_/¯
 var basePath = "https://image.tmdb.org/t/p/w500"; // Base path for posters
 var fallbackPath = "http://via.placeholder.com/500x700?text=No+Poster"; // Fallback poster art
 
@@ -39,7 +39,9 @@ Number.prototype.format = function(n, x) {
 var root = new Vue({
     el: "#root",
     data: {
-        tmdb: null, // Will hold an array of movies 
+        pages: null, // Total amount of pages for the search query
+        page: 1, // Which page you're on
+        movies: null, // Will hold an array of movies        
         genres: null, // Will hold an array of genres 
         query: null, // Bound to the searchbar
         selectedMovie: null // Set when a movie card is clicked, used by the modal
@@ -49,22 +51,26 @@ var root = new Vue({
             return encodeURIComponent(this.query)
         },
         results: function() {
-            return this.tmdb ? this.tmdb.length : 0;
+            return this.movies ? this.movies.length : 0;
         },
         resultsText: function() {
-            if (!this.tmdb || this.tmdb.length == 0) return "results";
-            return this.tmdb.length > 1 ? "results" : "result";
+            if (!this.movies || this.movies.length == 0) return "results";
+            return this.movies.length > 1 ? "results" : "result";
         }
     },
     methods: {
-        submitQuery: function() {
-            if (history) history.replaceState({}, this.query, `/search?q=${encodeURIComponent(this.query)}`);
+        submitQuery: function(page) {
+            this.page = page;
+            if (history) history.replaceState({}, this.query, `/search?q=${encodeURIComponent(this.query)}&page=${this.page}`);
             this.populateData();
         },
         populateData: function() {
-            var tmdbURI =  `https://api.themoviedb.org/3/search/movie?api_key=${tmdbKey}&query=${this.encodedQuery}&page=1&include_adult=false`;            
-            fetchJSON(tmdbURI)
-                .then((json) => root.tmdb = json.results)
+            var moviesURI =  `https://api.themoviedb.org/3/search/movie?api_key=${moviesKey}&query=${this.encodedQuery}&page=${this.page}&include_adult=false`;            
+            fetchJSON(moviesURI)
+                .then((json) => {
+                    root.pages = json.total_pages;
+                    root.movies = json.results;
+                })
                 .catch(console.error);
         },
         truncateReviewText: function(fullText) {
@@ -76,7 +82,7 @@ var root = new Vue({
         // The rest of these methods just manipulate movie properties for easy insertion into Vue templates
         getGenres: function(movie) {
             if (!movie.genre_ids) return;
-            // Compare the movie genres to the genres grabbed from TMDB
+            // Compare the movie genres to the genres grabbed from movies
             return root.genres
                 .filter(genre => movie.genre_ids.includes(genre.id))
                 .map(genre => genre.name)
@@ -92,23 +98,26 @@ var root = new Vue({
         }
     },
     watch: {
-        // When a movie is selected,
+        // When a movie is selected, asynchronously retrieve more data for it and display it once it's there
         selectedMovie: function(newMovie) {
             if (newMovie === null) return;
-            var movieURI = `https://api.themoviedb.org/3/movie/${newMovie.id}?api_key=${tmdbKey}&append_to_response=reviews`;
+            var movieURI = `https://api.themoviedb.org/3/movie/${newMovie.id}?api_key=${moviesKey}&append_to_response=reviews`;
             // Future improvement: Add caching to this
+            // Future improvement: Some kind of loading spinner in the modal while this data is being retrieved?
             fetchJSON(movieURI)
                 .then((json) => {
                     Object.assign(root.selectedMovie, json); // Merge the two JSON results together
-                    root.$forceUpdate();
-                });
+                    root.$forceUpdate(); // This will get Vue to recognize all the new properties now
+                })
+                .catch(console.error);
         }
     },
     created: function() {
         // If there's a query parameter (which there should be), use it
         var params = new URLSearchParams(window.location.search);
         if (params.has("q")) this.query = params.get("q");
-        var genresURI = `https://api.themoviedb.org/3/genre/movie/list?api_key=${tmdbKey}`;
+        if (params.has("page")) this.page = +params.get("page"); // + in front converts the result into a number
+        var genresURI = `https://api.themoviedb.org/3/genre/movie/list?api_key=${moviesKey}`;
         fetchJSON(genresURI)
             .then((json) => root.genres = json.genres)
             .then(() => {
